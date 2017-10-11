2b7dfa1096fa2049bbd3e1ff205329499ab1eb26
Carlo Del Don <carlo.deldon@gmail.com>
2017-10-11 17:15:55 +0200

    removed dead file

--- design.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'thermography_gui.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
-#
-# WARNING! All changes made in this file will be lost!
-
